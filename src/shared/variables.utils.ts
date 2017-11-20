b05755881397fa71b154b38861fd630500810f4c
Maique Rosa <maique.rosa@gmail.com>
2017-11-20 19:12:30 -0200

    alteracao de versao

--- src/shared/variables.utils.ts
+++ src/shared/variables.utils.ts
@@ -1,5 +1,5 @@
-//const marcia_express_api = 'http://localhost:3005/api/'
-const marcia_express_api = 'https://marciaexpress.tk/api/'
+const marcia_express_api = 'http://localhost:3005/api/'
+//const marcia_express_api = 'https://marciaexpress.tk/api/'
 const MONTHS = [
     'Janeiro',
     'Fevereiro',
